ed0a27cbb6ca4ad0ec574312d3c8c0d21e1104c3
Abdul Hakim Shifat <hakim.shifat@gmail.com>
2023-09-14 12:03:41 +0600

    Read

---
+++ Learning/52TSS/1.EvenOdd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    scanf("%d",&j);
+    for(i=1;i<=j;i++){
+        scanf("%d",&k);
+        if((k%2)==0){
+            printf("even\n");
+        }
+        else{
+            printf("odd\n");
+        }
+    }
+    return 0;
+}
