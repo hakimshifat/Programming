edfc93854eecf0f2e32f4c95a039a110b3453b48
Abdul Hakim Shifat <hakim.shifat@gmail.com>
2023-09-14 16:08:53 +0600

    done

--- Learning/52TSS/2.EvenOdd.c
+++ Learning/52TSS/2.EvenOdd.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    int i,k;
-    unsigned j;
-    scanf("%d",&j);
-    for(i=1;i<=j;i++){
-        scanf("%d",&k);
-        if((k%2)==0){
+    int i,j;
+    
+    char k[100];
+    
+    scanf("%d",&i);
+    
+    for(j=0;j<i;j++){
+    
+        scanf("%s",k);
+        int n=strlen(k);
+        int m=k[n-1];
+        if((m%2)==0){
             printf("even\n");
         }
         else{
             printf("odd\n");
         }
+
     }
     return 0;
 }
+
+
