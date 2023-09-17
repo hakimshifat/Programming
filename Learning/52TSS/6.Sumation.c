e91fb62d44d6c2a7025bdc02cf6097288313ad91
Abdul Hakim Shifat <hakim.shifat@gmail.com>
2023-09-17 13:37:03 +0600

    division and divisor

---
+++ Learning/52TSS/6.Sumation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    scanf("%d",&j);
+    for(i=0;i<j;i++){
+        scanf("%d",&k);
+        int m=k%10;
+        int n=k/10000;
+        printf("Sum = %d\n",m+n);
+    }
+    return 0;
+}
