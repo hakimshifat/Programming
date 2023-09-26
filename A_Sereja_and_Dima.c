1c5a9c71c11f58860f06fa62f87835473240fe85
Abdul Hakim Shifat <hakim.shifat@gmail.com>
2023-09-26 07:29:10 +0600

    works for 1 test case

--- A_Sereja_and_Dima.c
+++ A_Sereja_and_Dima.c
@@ -10,14 +10,15 @@ int main()
     }
     for(int m=1;m<=(j/2);m++){
     if(((m+1)%2)==0){
-        if(cards[m]>cards[j]){
+      //  if((j/2)==0){
+        if(cards[m]>cards[j+1-m]){
           //  temp[m]=cards[m];
           sum = sum + cards[m];
-          sum1 = sum1 + cards[j];
+          sum1 = sum1 + cards[j+1-m];
             //temp1[m]=cards[j];
         }
         else{
-           sum = sum + cards[j];
+           sum = sum + cards[j+1-m];
           sum1 = sum1 + cards[m];
            // temp[m]=cards[j];
            // temp1[m]=cards[m];
@@ -26,20 +27,22 @@ int main()
     else{
         if(cards[m]>cards[j]){
           sum1 = sum1 + cards[m];
-          sum = sum + cards[j];
+          sum = sum + cards[j+1-m];
           //  temp[m]=cards[m];
            // temp1[m]=cards[j];
         }
         else{
-        sum1 = sum1 + cards[j];
+        sum1 = sum1 + cards[j+1-m];
           sum = sum + cards[m];
           //  temp[m]=cards[j];
            // temp1[m]=cards[m];
+           sum1 = sum1 + (cards[1+j/2]/2);
+           sum = sum + (cards[1+j/2]/2);
         }
     } 
     }
     
-    printf("%d %d",sum1,sum);
+    printf("%d %d",sum,sum1);
 
     return 0;
 }
