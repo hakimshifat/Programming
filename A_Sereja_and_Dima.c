795ccd13047c02589a8525495a35ded998b01240
Abdul Hakim Shifat <hakim.shifat@gmail.com>
2023-09-26 09:24:51 +0600

    unsolved

--- A_Sereja_and_Dima.c
+++ A_Sereja_and_Dima.c
@@ -13,35 +13,39 @@ int main()
       //  if((j/2)==0){
         if(cards[m]>cards[j+1-m]){
           //  temp[m]=cards[m];
+          //j=j-1;
           sum = sum + cards[m];
-          sum1 = sum1 + cards[j+1-m];
+         // sum1 = sum1 + cards[j+1-m];
             //temp1[m]=cards[j];
         }
         else{
+        //m=m-1;
            sum = sum + cards[j+1-m];
-          sum1 = sum1 + cards[m];
+           
+         // sum1 = sum1 + cards[m];
            // temp[m]=cards[j];
            // temp1[m]=cards[m];
         }
     }
     else{
         if(cards[m]>cards[j]){
+        //j=j-1;
           sum1 = sum1 + cards[m];
-          sum = sum + cards[j+1-m];
+          
+         // sum = sum + cards[j+1-m];
           //  temp[m]=cards[m];
            // temp1[m]=cards[j];
         }
         else{
+        //m=m-1;
         sum1 = sum1 + cards[j+1-m];
-          sum = sum + cards[m];
+        
+        //  sum = sum + cards[m];
           //  temp[m]=cards[j];
            // temp1[m]=cards[m];
-           sum1 = sum1 + (cards[1+j/2]/2);
-           sum = sum + (cards[1+j/2]/2);
         }
     } 
     }
-    
     printf("%d %d",sum,sum1);
 
     return 0;
