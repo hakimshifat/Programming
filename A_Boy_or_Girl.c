11b5366407a38a3aeb2f00707e39289bd4c00967
Abdul Hakim Shifat <hakim.shifat@gmail.com>
2023-10-17 06:16:28 +0600

    solved

--- A_Boy_or_Girl.c
+++ A_Boy_or_Girl.c
@@ -5,18 +5,35 @@
 #define ll long long
 #define ld long double
 #define ull unsigned long long
-#define fo(i, n) for(int i=0;i<n;i++)
+#define fo(i,n) for(int i=0;i<n;i++)
 
-int commonchar(char arr[],int size){
+int commonchar(char arr[],int size){ //checks if how many common character a string has
     int count=0;
     fo(i,size-1){
         for(int u=i+1;u<size;u++){
             if(arr[i]==arr[u])
+             count++;
+           //  arr[i]=arr[i+1];
+          //   arr[u]=arr[u+1];
+        }
+    }
+return count;
+}
+
+int uc_s(char str[],int size){
+    int m[256]={0};
+    fo(i,size){
+        m[(int)str[i]]++;
+    }
+    int count=0;
+    fo(i,256){
+        if(m[i]>0){
             count++;
         }
     }
 return count;
 }
+
 
 void swap(int *xp,int *yp){ 
         int temp=*xp;
@@ -37,7 +54,7 @@ void selsort(int arr[],int size){ //sorting an array in accending order
 
 void inputarray(char arr[],int size){ // taking an array input
     fo(i,size){
-        scanf("%c",&arr[i]);
+        scanf("%[^\n]",&arr[i]);
     }
 }
 void printarray(char arr[],int size){ // printing the array
@@ -46,15 +63,17 @@ void printarray(char arr[],int size){ // printing the array
     }
 }
 void solve(){
-    //code here
     char str[100];
     inputarray(str,100);
+    //inputarray(str,100);
     int m,n;
     m=strlen(str);
-    printarray(str,m);
+  //  printarray(str,m);
    // n=commonchar(str,m);
-   printf(" %d",m);
-   // ((m-n)%2==0) ? printf("CHAT WITH HER!") : printf("IGNORE HIM!");
+   //printf("%d %d",m,n);
+  n=uc_s(str,m);
+  printf("%d",n);
+  //  ((m-n)%2==0) ? printf("CHAT WITH HER!") : printf("IGNORE HIM!");
 }
 int main()
 {
