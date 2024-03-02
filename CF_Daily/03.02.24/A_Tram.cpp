8ca20e5049742ab5ec948b0fd0d8ab435dcc89ad
Shifat <hakim.shifat@gmail.com>
2024-03-02 15:48:41 +0600

    accepted

--- CF_Daily/03.02.24/A_Tram.cpp
+++ CF_Daily/03.02.24/A_Tram.cpp
@@ -1,22 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int input, t; cin >> t;
+    int input, t; 
+    cin >> t;
     int sum = 0;
-    vector<int>trum;
-    vector<int>capt;
-    while(cin>>input){
+    vector<int> trum;
+    vector<int> array;
+
+    while (cin >> input) {
         trum.push_back(input);
     }
-    for(int i = 0;i<trum.size()-1;i+=2){
-        sum = sum + trum[i+1] - trum[i];
-     //   cout << sum <<" "<<endl ;
-        capt.push_back(sum);
+
+    for (int i = 0; i < trum.size() - 1; i += 2) {
+        sum += trum[i + 1] - trum[i];
+        array.push_back(sum);
     }
-    for(auto x:capt){
-        cout << capt[x];
-    }
+    sort(array.begin(),array.end(), greater<int>());
+    cout<< array[0];
+
     return 0;
 }
