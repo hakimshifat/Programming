9bedffd1e22a7c543e5dc672687bf5e33199deba
Sifat <hakim.shifat@gmail.com>
2024-03-15 01:07:34 +0600

    accepted

--- A_Team_Olympiad.cpp
+++ A_Team_Olympiad.cpp
@@ -3,14 +3,26 @@ using namespace std;
 
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    int t; cin >> t;
-    vector<int>k; int input;
-    while(cin >> input){
-        k.push_back(input);
-    }
-    for(auto x:k){
-        cout << x<<endl;
-    }
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+int input = 0;
+int left = 0, right = 0, sum = 0;
+int t = 0; cin >> t;
+while(t--){
+cin >> left >> right >> sum ;
+vector<int>leftCoin;
+while(left--){
+cin >> input;
+leftCoin.push_back(input);
+}
+vector<int>RightCoin;
+while(right--){
+cin >> input;
+RightCoin.push_back(input);
+}
+vector<int>denominator;
+cout << "\n";
+cout << left<<" "<< right<<" "<< sum<<" "<< leftCoin.size()<<" "<< RightCoin.size()<<endl;
+}
+return 0;
 }
---
+++ Timus_1409.cpp
@@ -0,0 +1,16 @@
+/*
+
+Author : Shifat
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int harry, larry; cin >> harry >> larry;
+    cout << larry-1 <<" "<<harry-1;
+    return 0;
+}
