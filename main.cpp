cff666bfe9bd64a4e1ad47b7d5eda08c28e73e14
Sifat <hakim.shifat@gmail.com>
2024-03-26 10:43:04 +0600

    Organized

--- main.cpp
+++ main.cpp
@@ -17,23 +17,5 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-       int num, n1=0, n2=1, nextTerm;
-
-    printf("Enter a number:");
-    scanf("%d",&num);
-
-    printf("Fibonacci series until %d:\n",num);
-    printf("%d\t %d\t",n1,n2);
-
-    nextTerm=n1+n2;
-    while(nextTerm<=num)
-    {
-        printf("%d\t",nextTerm);
-        n1=n2;
-        n2=nextTerm;
-        nextTerm=n1+n2;
-    }
-    printf("\n");
-
     return 0;
 }
